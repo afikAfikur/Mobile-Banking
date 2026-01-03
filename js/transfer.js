bc62f4132a82b5733a4f8d99396ad9f9df7c02e3
Afiqur <afikrahman28@gmsil.com>
2026-01-04 00:54:22 +0600

    only get bonus and transactions left

--- js/transfer.js
+++ js/transfer.js
@@ -25,3 +25,31 @@
       document.getElementById('logout').addEventListener('click',function(){
             window.location.href="../index.html"
         })
+        // here starts the code
+        let savedbalance = localStorage.getItem("upperbalance");
+document.getElementById("upperbalance").innerText = savedbalance;
+document
+  .getElementById("formcashout")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const agNum = document.getElementById("agenNumber").value;
+    const amount = document.getElementById("ammount").value;
+    const amnum = Number(amount);
+
+    const pin = document.getElementById("pin").value;
+    let bal = document.getElementById("upperbalance").innerText;
+
+    let numbal = Number(bal);
+
+    if (agNum.length == 11) {
+      if (pin == "1234") {
+        numbal = numbal - amnum;
+        numbalstring = String(numbal);
+        document.getElementById("upperbalance").innerText = numbalstring;
+      } else {
+        alert("Invalid Pin");
+      }
+    } else {
+      alert("Invalid Information");
+    }
+  });
